7bac8dabe8ba7843fb714a5ca332d17dc5a7d07a
root <root@Ubuntu2.myguest.virtualbox.org>
2023-07-07 22:40:07 +0800

    added a variable to control the type of output (client ip/ useragent)

--- ngx_http_fred_second_module.c
+++ ngx_http_fred_second_module.c
@@ -13,9 +13,31 @@ static void* ngx_http_fred_second_create_loc_conf(ngx_conf_t *cf);
 static char* ngx_http_fred_second_merge_loc_conf(ngx_conf_t *cf,
     void *parent, void *child);
 
+static void ngx_http_variable_set_fred_mode(ngx_http_request_t *r,
+     ngx_http_variable_value_t *v, uintptr_t data);
+
+static void ngx_http_variable_set_fred_mode(ngx_http_request_t *r,
+     ngx_http_variable_value_t *v, uintptr_t data);
+
+static ngx_int_t ngx_http_fred_second_add_variables(ngx_conf_t *cf);
+
+ngx_int_t
+ngx_fred_parse_mode(ngx_str_t *line);
+
 typedef struct {
     ngx_uint_t mode;
 } ngx_http_fred_second_loc_conf_t;
+
+// Defining my embedded variable
+static ngx_http_variable_t  ngx_http_fred_second_variables[] = {
+
+    { ngx_string("mode"), ngx_http_variable_set_fred_mode,
+      NULL,
+      0,
+      NGX_HTTP_VAR_CHANGEABLE|NGX_HTTP_VAR_NOCACHEABLE, 0 },
+    
+     ngx_http_null_variable
+};  
 
 /**
  * This module provided directive: hello world.
@@ -46,7 +68,7 @@ static ngx_command_t ngx_http_fred_second_commands[] = {
 
 /* The module context. */
 static ngx_http_module_t ngx_http_fred_second_module_ctx = {
-    NULL, /* preconfiguration */
+    ngx_http_fred_second_add_variables, /* preconfiguration */
     NULL, /* postconfiguration */
 
     NULL, /* create main configuration */
@@ -75,6 +97,72 @@ ngx_module_t ngx_http_fred_second_module = {
     NGX_MODULE_V1_PADDING
 };
 
+ngx_int_t
+ngx_fred_parse_mode(ngx_str_t *line)
+{
+    size_t   len;
+    ngx_uint_t mode;
+
+    len = line->len;
+
+    if (len == 0) {
+        return NGX_ERROR;
+    }
+
+    if (ngx_strcmp(line->data, "ua") == 0) {
+        mode = 1;
+        return mode;
+    } 
+    if (ngx_strcmp(line->data, "ip") == 0) {
+        mode = 0;
+        return mode;
+    }
+    return NGX_ERROR;
+}
+
+static void 
+ngx_http_variable_set_fred_mode(ngx_http_request_t *r,
+     ngx_http_variable_value_t *v, uintptr_t data) 
+{
+    ngx_http_fred_second_loc_conf_t  *fred_second_config;
+    fred_second_config = ngx_http_get_module_loc_conf(r, ngx_http_fred_second_module);
+    
+    ngx_int_t    s;
+    ngx_str_t  val;
+
+    val.len = v->len;
+    val.data = v->data;
+
+    s = ngx_fred_parse_mode(&val); // need declare this 
+
+    if (s == NGX_ERROR) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0,
+                      "invalid $mode \"%V\"", &val);
+        return;
+    }
+
+    fred_second_config->mode = (ngx_uint_t) s;
+}
+
+// Add it in preconfiguration
+static ngx_int_t
+ngx_http_fred_second_add_variables(ngx_conf_t *cf)
+{
+    ngx_http_variable_t  *var, *v;
+
+    for (v = ngx_http_fred_second_variables; v->name.len; v++) {
+        var = ngx_http_add_variable(cf, &v->name, v->flags);
+        if (var == NULL) {
+            return NGX_ERROR;
+        }
+        var->set_handler = v->set_handler;
+        var->get_handler = v->get_handler;
+        var->data = v->data;
+    }
+
+    return NGX_OK;
+}
+
 static void* ngx_http_fred_second_create_loc_conf(ngx_conf_t *cf)
 {
     ngx_http_fred_second_loc_conf_t *conf; // pointer to the location config struct
@@ -94,11 +182,11 @@ ngx_http_fred_second_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
     ngx_http_fred_second_loc_conf_t *prev = parent;
     ngx_http_fred_second_loc_conf_t *conf = child;
 
-    ngx_conf_merge_uint_value(conf->mode, prev->mode, 1);
+    ngx_conf_merge_uint_value(conf->mode, prev->mode, 0);
 
-    if (conf->mode < 1) {
+    if (conf->mode > 2) {
         ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, 
-            "value must be equal or more than 1");
+            "mode variable set incorrectly");
         return NGX_CONF_ERROR;
     }
 
@@ -166,7 +254,18 @@ ngx_http_fred_second_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 //} /* ngx_http_hello_world_handler */
 
 static ngx_int_t ngx_http_fred_second_handler(ngx_http_request_t *r)
-{
+{   
+    ngx_http_fred_second_loc_conf_t  *fred_second_config;
+    fred_second_config = ngx_http_get_module_loc_conf(r, ngx_http_fred_second_module);
+    // ngx_http_variable_value_t  *v;
+
+    // v = ngx_http_get_flushed_variable(r, index);
+
+    // if (v == NULL || v->not_found) {
+    //     /* we failed to get value or there is no such variable, handle it */
+    //     return NGX_ERROR;
+    // }   
+
     ngx_buf_t *b;
     ngx_chain_t out;
 
@@ -180,22 +279,59 @@ static ngx_int_t ngx_http_fred_second_handler(ngx_http_request_t *r)
     /* Insertion in the buffer chain. */
     out.buf = b;
     out.next = NULL; /* just one buffer */
+
     
-    ngx_str_t *ua;
-    ua = &r->headers_in.user_agent->value;
-    if (ua == NULL) {
-        return NGX_DECLINED;
+    
+    if (fred_second_config->mode == 1) {
+        ngx_str_t *ua;     
+        ua = &r->headers_in.user_agent->value;
+        if (ua == NULL) {
+            return NGX_DECLINED;
+        }
+
+        b->pos = ua->data; /* first position in memory of the data */
+        b->last = ua->data + ua->len; /* last position in memory of the data */
+        b->memory = 1; /* content is in read-only memory */
+        b->last_buf = 1; /* there will be no more buffers in the request */
+
+        /* Sending the headers for the reply. */
+        r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
+        /* Get the content length of the body. */
+        r->headers_out.content_length_n = ua->len;
+    } else if (fred_second_config->mode == 0) {
+        ngx_str_t *ip;
+        ip = &r->connection->addr_text;
+        if (ip == NULL) {
+            return NGX_DECLINED;
+        }
+
+        b->pos = ip->data; /* first position in memory of the data */
+        b->last = ip->data + ip->len; /* last position in memory of the data */
+        b->memory = 1; /* content is in read-only memory */
+        b->last_buf = 1; /* there will be no more buffers in the request */
+
+        /* Sending the headers for the reply. */
+        r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
+        /* Get the content length of the body. */
+        r->headers_out.content_length_n = ip->len;
+    } else {
+        ngx_str_t *host;     
+        host = &r->headers_in.host->value;
+        if (host == NULL) {
+            return NGX_DECLINED;
+        }
+
+        b->pos = host->data; /* first position in memory of the data */
+        b->last = host->data + host->len; /* last position in memory of the data */
+        b->memory = 1; /* content is in read-only memory */
+        b->last_buf = 1; /* there will be no more buffers in the request */
+
+        /* Sending the headers for the reply. */
+        r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
+        /* Get the content length of the body. */
+        r->headers_out.content_length_n = host->len;
     }
-
-    b->pos = ua->data; /* first position in memory of the data */
-    b->last = ua->data + ua->len; /* last position in memory of the data */
-    b->memory = 1; /* content is in read-only memory */
-    b->last_buf = 1; /* there will be no more buffers in the request */
-
-    /* Sending the headers for the reply. */
-    r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
-    /* Get the content length of the body. */
-    r->headers_out.content_length_n = ua->len;
+    
     ngx_http_send_header(r); /* Send the headers */
 
     /* Send the body, and return the status code of the output filter chain. */
